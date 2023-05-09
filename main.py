064fedc6ce8dfadb7391ecefd2e3c168d85dbf10
Alvaro Scheid <ascheid@gmail.com>
2023-05-09 15:26:21 -0400

    feat: better logging and telegram chat

--- main.py
+++ main.py
@@ -13,25 +13,85 @@ import os
 import ctypes
 import sys
 import logging
+from logging.handlers import RotatingFileHandler
+import requests
+import queue
+
+# Set the telegram chat id and bot token
+try:
+    TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+    TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+    USE_TELEGRAM = True
+except KeyError:
+    USE_TELEGRAM = False
+
+# Create a queue to hold the jobs
+telegram_queue = queue.Queue()
 
 # Set up logging configuration
 LOG_LEVEL = logging.DEBUG
-logging.basicConfig(
-    level=LOG_LEVEL,
-    format="%(asctime)s :%(levelname)s:%(funcName)s:%(lineno)d %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-)
+LOG_FILENAME = "blink_detector.log"
+LOG_MAX_BYTES = 10 * 1024 * 1024 # 10mb
+LOG_BACKUP_COUNT = 2
 
+# create a logger
+logger = logging.getLogger('')
+logger.setLevel(LOG_LEVEL)
+
+# create a file handler which logs even debug messages
+fh = RotatingFileHandler(LOG_FILENAME, maxBytes=LOG_MAX_BYTES, backupCount=LOG_BACKUP_COUNT)
+fh.setLevel(LOG_LEVEL)
+
+# create a console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(LOG_LEVEL)
+
+# create a formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s :%(levelname)s:%(funcName)s:%(lineno)d %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+
+def send_telegram_photo(chat_id, photo, bot_token, message):
+    url = f"https://api.telegram.org/bot{bot_token}/sendPhoto?chat_id=-{chat_id}&caption={message}"
+    try:
+        with open(photo, 'rb') as f:
+            files = {'photo': f}
+            data = {'chat_id': chat_id,
+                    'caption': message}
+            response = requests.post(url, files=files)
+            response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        logger.error(f"HTTP error occurred when sending telegram photo: {e}")
+    finally:
+        os.remove(photo)
+
+def send_telegram_photo_thread(chat_id, photo, bot_token, message):
+    telegram_queue.put((chat_id, photo, bot_token, message))
+
+
+def send_telegram_photo_worker():
+    while True:
+        job = telegram_queue.get()
+        if job is None:
+            break
+        chat_id, photo, bot_token, message = job
+        send_telegram_photo(chat_id, photo, bot_token, message)
+        telegram_queue.task_done()
 
 
 def play_alarm(stop_event):
     # play an alarm sound and keep playing it until the stop event is set
     while not stop_event.is_set():
-        logging.info("playing alarm.wav")
+        logger.info("playing alarm.wav")
         if sys.platform.startswith('win'):
             # Play the default sound for Windows
             ctypes.windll.user32.MessageBeep(-1)
-            time.sleep(0.5)
+            time.sleep(1)
         elif sys.platform.startswith('darwin'):
             # Play the default sound for macOS
             os.system("afplay /System/Library/Sounds/Glass.aiff")
@@ -41,7 +101,7 @@ def play_alarm(stop_event):
             os.system("aplay /usr/share/sounds/gnome/default/alerts/glass.ogg")
             time.sleep(0.5)
         else:
-            logging.error("Unsupported platform")
+            logger.error("Unsupported platform")
             raise ValueError("Unsupported platform")
 
 
@@ -76,6 +136,7 @@ def main():
     args = vars(ap.parse_args())
     EYE_AR_THRESH = args['threshold']
     EYE_AR_CONSEC_FRAMES = args['frames']
+    PROGRAM_ENABLE = True
     ALARM_ON = False
     ALARM_THREAD = None
     ALARM_STOP_EVENT = threading.Event()    
@@ -87,7 +148,7 @@ def main():
 
     # initialize dlib's face detector (HOG-based) and then create
     # the facial landmark predictor
-    logging.debug("loading facial landmark predictor...")
+    logger.debug("loading facial landmark predictor...")
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(args["shape_predictor"])
  
@@ -97,8 +158,8 @@ def main():
     (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
     
     # start the video stream thread
-    logging.info("starting video stream thread...")
-    logging.info("print q to quit...")
+    logger.info("starting video stream thread...")
+    logger.info("print q to quit...")
 
     if args['video'] == "camera":
         vs = VideoStream(src=0).start()
@@ -110,7 +171,12 @@ def main():
     time.sleep(1.0)
     
     # loop over frames from the video stream
-    logging.debug("starting main loop...")
+    logger.debug("starting main loop...")
+
+    # Start the worker thread
+    logger.debug("starting telegram worker thread...")
+    telegram_thread = threading.Thread(target=send_telegram_photo_worker)
+    telegram_thread.start()
     while True:
     	# if this is a file video stream, then we need to check if
     	# there any more frames left in the buffer to process
@@ -161,10 +227,10 @@ def main():
             else:
     			# if the eyes were closed for a sufficient number of
     			# then increment the total number of blinks
-                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                if COUNTER >= EYE_AR_CONSEC_FRAMES and PROGRAM_ENABLE:
                     TOTAL += 1
                     BLINK_TIMESTAMPS.append(time.time())
-                    logging.debug(f"blink detected! blinks in the past 5s: {len(BLINK_TIMESTAMPS)}")
+                    logger.debug(f"blink detected! blinks in the past 5s: {len(BLINK_TIMESTAMPS)}")
     
     			# reset the eye frame counter
                 COUNTER = 0
@@ -179,27 +245,37 @@ def main():
     			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             cv2.putText(frame, "Alarm: {}".format(ALARM_ON), (10, 60),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "Program Enabled: {}".format(PROGRAM_ENABLE), (10, 90),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             
             # function to trigger alarm when user blinks more than 5 times in 5 seconds and alarm is not already on
-            if len(BLINK_TIMESTAMPS) >= 5 and not ALARM_ON:
-                logging.debug("alarm triggered!")
+            if len(BLINK_TIMESTAMPS) >= 5 and not ALARM_ON and PROGRAM_ENABLE:
+                logger.debug("alarm triggered!")
                 ALARM_ON = True
                 ALARM_STOP_EVENT.clear()
                 ALARM_THREAD = threading.Thread(target=play_alarm, args=(ALARM_STOP_EVENT,))
                 ALARM_THREAD.start()
                 BLINK_TIMESTAMPS = []
                 TOTAL = 0
+                if USE_TELEGRAM:
+                    cv2.imwrite("frame.jpg", frame)
+                    message = "Alarm triggered at {0}".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                    send_telegram_photo_thread(TELEGRAM_CHAT_ID, "frame.jpg", TELEGRAM_BOT_TOKEN, message)
 
             # if the alarm is on and the user blinks 5 times in 5 seconds, turn off the alarm
             # killing the thread running the alarm, and reset the blink timestamps and total
             if len(BLINK_TIMESTAMPS) >= 5 and ALARM_ON:
-                 logging.debug("alarm stopped!")
+                 logger.debug("alarm stopped!")
                  ALARM_ON = False
                  ALARM_STOP_EVENT.set()
                  if ALARM_THREAD is not None:
                      ALARM_THREAD.join()
                  BLINK_TIMESTAMPS = []
                  TOTAL = 0
+                 if USE_TELEGRAM:
+                    cv2.imwrite("frame.jpg", frame)
+                    message = "Alarm stopped at {0}".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                    send_telegram_photo_thread(TELEGRAM_CHAT_ID, "frame.jpg", TELEGRAM_BOT_TOKEN, message)
      
     	# show the frame
         cv2.imshow("Frame", frame)
@@ -207,11 +283,27 @@ def main():
      
     	# if the `q` key was pressed, break from the loop
         if key == ord("q"):
+            if USE_TELEGRAM:
+                    cv2.imwrite("frame.jpg", frame)
+                    message = "q pressed at {0}".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                    send_telegram_photo_thread(TELEGRAM_CHAT_ID, "frame.jpg", TELEGRAM_BOT_TOKEN, message)
             break
+        
+        # if the `s` key was pressed, toggle program enable
+        if key == ord("s"):
+            PROGRAM_ENABLE = not PROGRAM_ENABLE
+            logger.info(f"s key pressed. program enable: {PROGRAM_ENABLE}")
+            if USE_TELEGRAM:
+                    cv2.imwrite("frame.jpg", frame)
+                    message = "q pressed at {0}".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+                    send_telegram_photo_thread(TELEGRAM_CHAT_ID, "frame.jpg", TELEGRAM_BOT_TOKEN, message)
     
     # do a bit of cleanup
     cv2.destroyAllWindows()
     vs.stop()
+    telegram_queue.put(None)
+    telegram_thread.join()
+
 
 if __name__ == '__main__' :
     main()
